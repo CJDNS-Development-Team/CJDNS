3e9a8dec14a61051306553939e52a4bd6de9894e
Eenae <alexey@mixbytes.io>
2020-06-03 19:56:44 +0300

    applied rustfmt

--- cjdns-splice/src/lib.rs
+++ cjdns-splice/src/lib.rs
@@ -1,37 +1,34 @@
 use std::error;
 use std::fmt;
 
-use cjdns_entities::{LabelT, Label};
-
+use cjdns_entities::{Label, LabelT};
 
 /// Result type alias.
 pub type Result<T> = std::result::Result<T, Error>;
 
-
 /// Error type.
 #[derive(Debug)]
 pub enum Error {
-    LabelTooLong
+    LabelTooLong,
 }
-
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::LabelTooLong => write!(f, "Label is too long")
+            Error::LabelTooLong => write!(f, "Label is too long"),
         }
     }
 }
 
 impl error::Error for Error {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> { None }
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
 }
-
 
 pub fn splice<L: LabelT>(labels: &[L]) -> Result<L> {
     Ok(labels[0] ^ labels[1])
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -39,7 +36,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(splice(&[Label::new(3), Label::new(1), Label::new(5)]).unwrap(),
-                   Label::new(2));
+        assert_eq!(
+            splice(&[Label::new(3), Label::new(1), Label::new(5)]).unwrap(),
+            Label::new(2)
+        );
     }
 }
