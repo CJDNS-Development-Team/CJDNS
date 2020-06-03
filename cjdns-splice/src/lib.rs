d8b1c116411570845ea93e21989dd57b5fe6720e
Eenae <alexey@mixbytes.io>
2020-06-03 19:34:33 +0300

    error struct

--- cjdns-splice/src/lib.rs
+++ cjdns-splice/src/lib.rs
@@ -1,8 +1,35 @@
+use std::error;
+use std::fmt;
+
 use cjdns_entities::{LabelT, Label};
 
 
-pub fn splice<L: LabelT>(labels: &[L]) -> L {
-    labels[0] ^ labels[1]
+/// Result type alias.
+pub type Result<T> = std::result::Result<T, Error>;
+
+
+/// Error type.
+#[derive(Debug)]
+pub enum Error {
+    LabelTooLong
+}
+
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::LabelTooLong => write!(f, "Label is too long")
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> { None }
+}
+
+
+pub fn splice<L: LabelT>(labels: &[L]) -> Result<L> {
+    Ok(labels[0] ^ labels[1])
 }
 
 
@@ -12,7 +39,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(splice(&[Label::new(3), Label::new(1), Label::new(5)]),
+        assert_eq!(splice(&[Label::new(3), Label::new(1), Label::new(5)]).unwrap(),
                    Label::new(2));
     }
 }
