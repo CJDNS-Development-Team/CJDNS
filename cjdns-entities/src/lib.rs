339b86ca819a0e96de5fc7cc0666ff1cdbcae378
Eenae <alexey@mixbytes.io>
2020-06-05 19:24:30 +0300

    encoding schemes

--- cjdns-entities/src/lib.rs
+++ cjdns-entities/src/lib.rs
@@ -2,12 +2,15 @@
 extern crate lazy_static;
 extern crate regex;
 
+use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::fmt;
+use std::iter::IntoIterator;
 use std::mem::size_of;
 use std::ops::{BitAnd, BitOr, BitXor, Shl, Shr};
 use std::string::ToString;
 use std::u64;
+use std::vec::Vec;
 
 use regex::Regex;
 
@@ -46,6 +49,98 @@ pub struct Label64(u64);
 
 /// 128 bit label.
 //pub struct Label128(u128);
+
+/// Form used in an encoding scheme.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct EncodingSchemeForm {
+    pub bit_count: u8,
+    pub prefix_len: u8,
+    pub prefix: u8,
+}
+
+/// Encoding scheme.
+/// Schemes are comparable for equality, immutable, opaque and iterable.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct EncodingScheme(Vec<EncodingSchemeForm>);
+
+lazy_static! {
+    static ref SCHEMES: HashMap<&'static str, EncodingScheme> = {
+        let mut m = HashMap::new();
+
+        m.insert(
+            "f4",
+            EncodingScheme::new(&[EncodingSchemeForm {
+                bit_count: 4,
+                prefix_len: 0,
+                prefix: 0,
+            }]),
+        );
+
+        m.insert(
+            "f8",
+            EncodingScheme::new(&[EncodingSchemeForm {
+                bit_count: 8,
+                prefix_len: 0,
+                prefix: 0,
+            }]),
+        );
+
+        m.insert(
+            "v48",
+            EncodingScheme::new(&[
+                EncodingSchemeForm {
+                    bit_count: 4,
+                    prefix_len: 1,
+                    prefix: 0b01,
+                },
+                EncodingSchemeForm {
+                    bit_count: 8,
+                    prefix_len: 1,
+                    prefix: 0b00,
+                },
+            ]),
+        );
+
+        m.insert(
+            "v358",
+            EncodingScheme::new(&[
+                EncodingSchemeForm {
+                    bit_count: 3,
+                    prefix_len: 1,
+                    prefix: 0b01,
+                },
+                EncodingSchemeForm {
+                    bit_count: 5,
+                    prefix_len: 2,
+                    prefix: 0b10,
+                },
+                EncodingSchemeForm {
+                    bit_count: 8,
+                    prefix_len: 2,
+                    prefix: 0b00,
+                },
+            ]),
+        );
+
+        m.insert(
+            "v37",
+            EncodingScheme::new(&[
+                EncodingSchemeForm {
+                    bit_count: 3,
+                    prefix_len: 1,
+                    prefix: 0b01,
+                },
+                EncodingSchemeForm {
+                    bit_count: 7,
+                    prefix_len: 1,
+                    prefix: 0b00,
+                },
+            ]),
+        );
+
+        m
+    };
+}
 
 impl Label64 {
     pub fn new(v: u64) -> Self {
@@ -157,6 +252,28 @@ impl LabelT for Label64 {
     }
 }
 
+impl EncodingScheme {
+    pub fn new(forms: &[EncodingSchemeForm]) -> Self {
+        let mut v = forms.to_vec();
+        v.sort(); // schemes are comparable; order of forms doesn't matter
+        v.dedup();
+        Self(v)
+    }
+
+    pub fn forms(&self) -> &Vec<EncodingSchemeForm> {
+        &self.0
+    }
+}
+
+impl IntoIterator for EncodingScheme {
+    type Item = EncodingSchemeForm;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     extern crate rand;
@@ -168,6 +285,14 @@ mod tests {
 
     fn l64(v: u64) -> Label64 {
         Label64::new(v)
+    }
+
+    fn eform(bit_count: u8, prefix_len: u8, prefix: u8) -> EncodingSchemeForm {
+        EncodingSchemeForm {
+            bit_count,
+            prefix_len,
+            prefix,
+        }
     }
 
     #[test]
@@ -220,5 +345,53 @@ mod tests {
         assert_eq!(l64(2).highest_set_bit().unwrap(), 1u32);
         assert_eq!(l64(14574489829).highest_set_bit().unwrap(), 33u32);
         assert_eq!(l64(1u64 << 63).highest_set_bit().unwrap(), 63u32);
+    }
+
+    #[test]
+    fn encoding_scheme_comparison() {
+        assert_eq!(
+            EncodingScheme::new(&[eform(4, 2, 0b00)]),
+            EncodingScheme::new(&[eform(4, 2, 0b00)])
+        );
+        assert_eq!(
+            EncodingScheme::new(&[eform(4, 2, 0b00)]),
+            EncodingScheme::new(&[eform(4, 2, 0b00), eform(4, 2, 0b00)])
+        );
+        assert_eq!(
+            EncodingScheme::new(&[eform(4, 2, 0b00), eform(4, 2, 0b01)]),
+            EncodingScheme::new(&[eform(4, 2, 0b00), eform(4, 2, 0b01), eform(4, 2, 0b00)])
+        );
+
+        assert_ne!(
+            EncodingScheme::new(&[eform(4, 2, 0b00)]),
+            EncodingScheme::new(&[eform(4, 2, 0b01)])
+        );
+        assert_ne!(
+            EncodingScheme::new(&[eform(4, 2, 0b00)]),
+            EncodingScheme::new(&[eform(4, 2, 0b10)])
+        );
+        assert_ne!(
+            EncodingScheme::new(&[eform(4, 2, 0b00)]),
+            EncodingScheme::new(&[eform(3, 2, 0b00)])
+        );
+        assert_ne!(
+            EncodingScheme::new(&[eform(4, 2, 0b00)]),
+            EncodingScheme::new(&[eform(4, 2, 0b00), eform(4, 2, 0b01)])
+        );
+    }
+
+    #[test]
+    fn encoding_scheme_iteration() {
+        assert_eq!(
+            EncodingScheme::new(&[eform(4, 2, 0b00), eform(4, 2, 0b01)])
+                .into_iter()
+                .collect::<Vec<EncodingSchemeForm>>(),
+            vec![eform(4, 2, 0b00), eform(4, 2, 0b01)]
+        );
+    }
+
+    #[test]
+    fn schemes() {
+        assert_eq!(*SCHEMES.get("f8").unwrap().forms(), vec![eform(8, 0, 0)])
     }
 }
