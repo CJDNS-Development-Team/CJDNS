0a41b79b86f875ae1d46be5aba8f0be09fabcb7e
Alex Kordys <a.kordys@mixbytes.io>
2020-10-19 04:24:08 +0300

    Msgpack serialization rewritten

--- cjdns-snode/src/message.rs
+++ cjdns-snode/src/message.rs
@@ -1,8 +1,6 @@
 //! Msgpack-encodable message that is sent between cjdns supernodes.
 
-use std::collections::VecDeque;
-
-use rmpv::{decode, encode, Utf8String, Value};
+use rmpv::{decode::read_value, encode::write_value, Value};
 use thiserror::Error;
 
 use cjdns_ann::AnnHash;
@@ -28,63 +26,76 @@ pub enum MessageData {
 
 #[derive(Error, Clone, PartialEq, Eq, Debug)]
 pub enum EncodingError {
-    #[error("Message encoding ended up with an error: {0}")]
-    CannotWriteMessageToBuf(String)
+    #[error("Failed to serialize MsgPack message: {0}")]
+    MsgpackSerializeError(String)
 }
 
 #[derive(Error, Clone, PartialEq, Eq, Debug)]
 pub enum DecodingError {
-    #[error("Message decoding ended up with an error: {0}")]
-    CannotReadMessageFromBuf(String),
+    #[error("Failed to deserialize MsgPack message: {0}")]
+    MsgpackDeserializeError(String),
 
-    #[error("Received value type can't be used to decode message")]
-    InvalidMsgPackValueType,
+    #[error("Bad message: Array of length 2..4 expected at the root")]
+    BadMessageRootType,
 
-    #[error("Conversion from message pack value to intended type failed")]
-    CannotConvertFromMsgPackValue,
+    #[error("Bad message: Array of length 2..4 expected at the root, got {0}")]
+    BadMessageRootArrayLength(usize),
 
-    #[error("Received message pack array with invalid length")]
-    InvalidMsgPackArrayLength,
+    #[error("Bad message: ID field expected to be numeric")]
+    BadIdFieldType,
 
-    #[error("Received message with unrecognized type and data")]
-    UnrecognizedMessage,
+    #[error("Bad message: TYPE field expected to be String")]
+    BadTypeFieldType,
 
-    #[error("Message type value isn't utf-8 string")]
-    InvalidMessageTypeEncoding
+    #[error("Bad message: `{0}` message expected to have {1} args but found {2}")]
+    BadArgsCount(String, usize, usize),
+
+    #[error("Bad message: `{0}` message's arg(s) have wrong type(s)")]
+    BadArgType(String),
+
+    #[error("Bad HELLO message: VERSION field expected to be numeric")]
+    BadHelloVersionFieldType,
+
+    #[error("Bad message: `{0}` not recognized")]
+    UnrecognizedMessageType(String),
 }
 
 impl Message {
     pub fn decode_msgpack(bytes: &[u8]) -> Result<Self, DecodingError> {
-        let msg = decode::read_value(&mut &bytes[..]).map_err(|e| DecodingError::CannotReadMessageFromBuf(e.to_string()))?;
-        Ok(Self::from_msgpack_value(msg)?)
+        let msg = read_value(&mut &bytes[..]).map_err(|e| DecodingError::MsgpackDeserializeError(e.to_string()))?;
+        Self::from_msgpack(msg)
     }
 
     pub fn encode_msgpack(&self) -> Result<Vec<u8>, EncodingError> {
+        let msg = self.as_msgpack();
         let mut res = Vec::new();
-        let msg = self.as_msgpack_value();
-        encode::write_value(&mut res, &msg).map_err(|e| EncodingError::CannotWriteMessageToBuf(e.to_string()))?;
+        write_value(&mut res, &msg).map_err(|e| EncodingError::MsgpackSerializeError(e.to_string()))?;
         Ok(res)
     }
 
-    fn from_msgpack_value(msg: Value) -> Result<Self, DecodingError> {
-        if let Value::Array(mut msg_array) = msg {
-            if msg_array.len() <= 1 || msg_array.len() > 4 {
-                return Err(DecodingError::InvalidMsgPackArrayLength);
+    fn from_msgpack(msg: Value) -> Result<Self, DecodingError> {
+        if let Value::Array(root_arr) = msg {
+            let n = root_arr.len();
+            if n < 2 || n > 4 {
+                return Err(DecodingError::BadMessageRootArrayLength(n));
             }
-            let id = msg_array.remove(0).as_u64().ok_or(DecodingError::CannotConvertFromMsgPackValue)?;
-            let data = MessageData::from_msgpack_value(msg_array)?;
+            let id = root_arr[0].as_u64().ok_or(DecodingError::BadIdFieldType)?;
+            let data = MessageData::from_msgpack(&root_arr[1..])?;
             Ok(Message(id, data))
         } else {
-            return Err(DecodingError::InvalidMsgPackValueType);
+            return Err(DecodingError::BadMessageRootType);
         }
     }
 
-    fn as_msgpack_value(&self) -> Value {
-        let id = Value::from(self.0);
-        let data = MessageData::as_msgpack_values(&self.1);
-        let mut res = Vec::with_capacity(1 + data.len());
+    fn as_msgpack(&self) -> Value {
+        let Message(id, ref data) = *self;
+        let mut res = Vec::with_capacity(4);
+
+        let id = Value::from(id);
         res.push(id);
-        res.extend_from_slice(&data);
+
+        let res = data.as_msgpack(res);
+
         Value::Array(res)
     }
 }
@@ -108,97 +119,135 @@ impl MessageData {
         Some(res)
     }
 
-    /// `msg_data` length is checked in `Message::from_msgpack_value`
-    fn from_msgpack_value(msg_data: Vec<Value>) -> Result<Self, DecodingError> {
-        let mut msg_data = VecDeque::from(msg_data);
-        let type_str = msg_data.pop_front().expect("internal error: message data without message type");
-        let data = msg_data.pop_back();
+    fn from_msgpack(msg_data: &[Value]) -> Result<Self, DecodingError> {
+        debug_assert!(msg_data.len() >= 1 && msg_data.len() <= 4); // checked by the caller
+        let type_str = if let Value::String(s) = &msg_data[0] {
+            s.as_str().ok_or(DecodingError::BadTypeFieldType)?
+        } else {
+            return Err(DecodingError::BadTypeFieldType);
+        };
+        let data = &msg_data[1..];
+        let n = data.len();
+
+        let check_data_len = |expected_len: usize| -> Result<(), DecodingError> {
+            if n != expected_len {
+                Err(DecodingError::BadArgsCount(type_str.to_string(), expected_len, n))
+            } else {
+                Ok(())
+            }
+        };
+
         match type_str {
-            Value::String(msg_type) if msg_type == Utf8String::from("PING") && data.is_none() => {
+            "PING" => {
+                check_data_len(0)?;
                 Ok(MessageData::PING)
             }
-            Value::String(msg_type) if msg_type == Utf8String::from("ACK") && data.is_none() => {
+
+            "ACK" => {
+                check_data_len(0)?;
                 Ok(MessageData::ACK)
             }
-            Value::String(msg_type) => {
-                if data.is_none() {
-                    return Err(DecodingError::UnrecognizedMessage)
+
+            "HELLO" => {
+                check_data_len(1)?;
+                let ver = data[0].as_u64().ok_or(DecodingError::BadHelloVersionFieldType)?;
+                Ok(MessageData::HELLO(ver))
+            }
+
+            "OLLEH" => {
+                check_data_len(1)?;
+                let ver = data[0].as_u64().ok_or(DecodingError::BadHelloVersionFieldType)?;
+                Ok(MessageData::OLLEH(ver))
+            }
+
+            "GET_DATA" => {
+                check_data_len(1)?;
+                if let Value::Binary(hash) = &data[0] {
+                    if hash.len() > 0 { // Hash str can't be empty
+                        return Ok(MessageData::GET_DATA(AnnHash(hash.clone())));
+                    }
                 }
-                match data.expect("internal error: data is none") {
-                    Value::Integer(int) if msg_type == Utf8String::from("HELLO") => {
-                        let a = int.as_u64().ok_or(DecodingError::CannotConvertFromMsgPackValue)?;
-                        Ok(MessageData::HELLO(a))
+                return Err(DecodingError::BadArgType(type_str.to_string()));
+            }
+
+            "DATA" => {
+                check_data_len(1)?;
+                match &data[0] {
+                    Value::Binary(data) => {
+                        // Empty data is allowed
+                        Ok(MessageData::DATA(data.clone()))
                     }
-                    Value::Integer(int) if msg_type == Utf8String::from("OLLEH") => {
-                        let a = int.as_u64().ok_or(DecodingError::CannotConvertFromMsgPackValue)?;
-                        Ok(MessageData::OLLEH(a))
+                    Value::Nil => {
+                        // Nil data is allowed (same as empty)
+                        Ok(MessageData::DATA(Vec::new()))
                     }
-                    Value::Binary(buf) if msg_type == Utf8String::from("GET_DATA") && !buf.is_empty() => {
-                        Ok(MessageData::GET_DATA(AnnHash(buf)))
+                    _ => {
+                        trace!(">>> {:?}", msg_data);
+                        Err(DecodingError::BadArgType(type_str.to_string()))
                     }
-                    Value::Binary(buf) if msg_type == Utf8String::from("DATA") && !buf.is_empty() => {
-                        Ok(MessageData::DATA(buf))
-                    }
-                    Value::Array(arr) if msg_type == Utf8String::from("INV") && !arr.is_empty() => {
-                        let mut ret = Vec::new();
-                        for val in arr {
-                            match val {
-                                Value::Binary(buf) if !buf.is_empty() => ret.push(AnnHash(buf)),
-                                _ => return Err(DecodingError::InvalidMsgPackValueType)
-                            }
-                        }
-                        Ok(MessageData::INV(ret))
-                    }
-                    _ => Err(DecodingError::UnrecognizedMessage)
                 }
             }
-            _ => Err(DecodingError::InvalidMessageTypeEncoding)
+
+            "INV" => {
+                check_data_len(2)?;
+                let arr = data[1].as_array().ok_or(DecodingError::BadArgType(type_str.to_string()))?;
+                let try_hashes: Result<_, _> = arr
+                    .iter()
+                    .map(|val| {
+                        if let Value::Binary(hash) = val {
+                            if hash.len() > 0 {
+                                Ok(AnnHash(hash.clone()))
+                            } else {
+                                Err(DecodingError::BadArgType(type_str.to_string()))
+                            }
+                        } else {
+                            Err(DecodingError::BadArgType(type_str.to_string()))
+                        }
+                    })
+                    .collect();
+                let hashes = try_hashes?;
+                Ok(MessageData::INV(hashes))
+            }
+
+            _ => Err(DecodingError::UnrecognizedMessageType(type_str.to_string()))
         }
     }
 
-    fn as_msgpack_values(&self) -> Vec<Value> {
-        let mut ret = Vec::with_capacity(2);
+    fn as_msgpack(&self, mut res: Vec<Value>) -> Vec<Value> {
         match self {
             MessageData::HELLO(a) => {
-                let type_str = Value::from("HELLO");
-                let int_arg = Value::from(*a);
-                ret.push(type_str);
-                ret.push(int_arg);
+                res.push(Value::from("HELLO"));
+                res.push(Value::from(*a));
             },
             MessageData::OLLEH(a) => {
-                let type_str = Value::from("OLLEH");
-                let int_arg = Value::from(*a);
-                ret.push(type_str);
-                ret.push(int_arg);
+                res.push(Value::from("OLLEH"));
+                res.push(Value::from(*a));
             },
             MessageData::PING => {
-                let type_str = Value::from("PING");
-                ret.push(type_str);
+                res.push(Value::from("PING"));
             },
             MessageData::ACK => {
-                let type_str = Value::from("ACK");
-                ret.push(type_str);
+                res.push(Value::from("ACK"));
             },
             MessageData::GET_DATA(data) => {
-                let type_str = Value::from("GET_DATA");
-                let arr_arg = Value::from(data.bytes());
-                ret.push(type_str);
-                ret.push(arr_arg);
+                res.push(Value::from("GET_DATA"));
+                res.push(Value::from(data.bytes()));
             },
             MessageData::DATA(data) => {
-                let type_str = Value::from("DATA");
-                let arr_arg = Value::from(data.as_slice());
-                ret.push(type_str);
-                ret.push(arr_arg);
+                res.push(Value::from("DATA"));
+                if data.is_empty() {
+                    res.push(Value::Nil);
+                } else {
+                    res.push(Value::from(data.as_slice()));
+                }
             },
             MessageData::INV(data) => {
-                let type_str = Value::from("INV");
-                let arr_arg = data.iter().map(|v| Value::from(v.bytes())).collect();
-                ret.push(type_str);
-                ret.push(arr_arg);
+                res.push(Value::from("INV"));
+                res.push(Value::from(0)); // Dummy 0 integer
+                res.push(data.iter().map(|v| Value::from(v.bytes())).collect());
             },
         }
-        ret
+        res
     }
 }
 
@@ -249,12 +298,16 @@ mod tests {
     }
 
     #[test]
-    fn test_message_simple() {
+    fn test_message_encode_decode() {
         let version = 0x42;
 
         let test = |msg: Message| {
-            let bytes = msg.encode_msgpack().expect("failed writing value to buf");
-            let decoded = Message::decode_msgpack(&bytes).expect("failed decoding");
+            let try_encoded = msg.encode_msgpack();
+            assert_eq!(try_encoded.as_ref().err(), None, "failed to encode {:?}", msg);
+            let encoded = try_encoded.unwrap();
+            let try_decoded = Message::decode_msgpack(&encoded);
+            assert_eq!(try_decoded.as_ref().err(), None, "failed to decode {:?}", hex::encode(encoded));
+            let decoded = try_decoded.unwrap();
             assert_eq!(msg, decoded, "msgpack encode/decode test failed for message {:?}", msg);
         };
 
@@ -268,12 +321,16 @@ mod tests {
     }
 
     #[test]
-    fn test_message_real() {
+    fn test_message_decode_encode() {
         let test = |msg: Message, bytes: &[u8]| {
-            let decoded = Message::decode_msgpack(bytes).expect("failed decoding");
+            let try_decoded = Message::decode_msgpack(bytes);
+            assert_eq!(try_decoded.as_ref().err(), None, "failed to decode {:?}", hex::encode(bytes));
+            let decoded = try_decoded.unwrap();
             assert_eq!(msg, decoded, "msgpack decode test failed for message {:?}", msg);
 
-            let encoded = msg.encode_msgpack().expect("failed writing value to buf");
+            let try_encoded = msg.encode_msgpack();
+            assert_eq!(try_encoded.as_ref().err(), None, "failed to encode {:?}", msg);
+            let encoded = try_encoded.unwrap();
             assert_eq!(encoded, bytes, "msgpack encode test failed for message {:?}", msg);
         };
 
@@ -283,32 +340,33 @@ mod tests {
         test(msg![1, "ACK"], hex!("9201a341434b"));
         test(msg![2, "GET_DATA" | hash = hash![0xA1, 0xB2, 0xC3, 0xD4]], hex!("9302a84745545f44415441c404a1b2c3d4"));
         test(msg![2, "DATA" | data = vec![0xA1, 0xB2, 0xC3, 0xD4]], hex!("9302a444415441c404a1b2c3d4"));
-        test(msg![0, "INV", 0 => hashes = &[ hash![0xA1, 0xB2, 0xC3, 0xD4], hash![0xE5, 0xF6, 0x01, 0x02], hash![0x12, 0x34, 0x56, 0x78] ]], hex!("9300a3494e5693c404a1b2c3d4c404e5f60102c40412345678"));
+        test(msg![0, "INV", 0 => hashes = &[ hash![0xA1, 0xB2, 0xC3, 0xD4], hash![0xE5, 0xF6, 0x01, 0x02], hash![0x12, 0x34, 0x56, 0x78] ]], hex!("9400a3494e560093c404a1b2c3d4c404e5f60102c40412345678"));
     }
 
     #[test]
     fn test_message_decode_err() {
         let test = |bytes:  &[u8], err: DecodingError| {
-            let decoded_err = Message::decode_msgpack(bytes).expect_err("valid message bytes");
-            assert_eq!(decoded_err, err, "msgpack decode test failed for bytes {:X?}", bytes);
+            let decoded_err = Message::decode_msgpack(bytes).expect_err("decode not failed");
+            assert_eq!(decoded_err, err, "decode failed with different error: {}", hex::encode(bytes));
         };
 
-        test(&[0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f], DecodingError::InvalidMsgPackValueType);
-        // len == 1
-        test(&[0x91, 0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f], DecodingError::InvalidMsgPackArrayLength);
+        // not array
+        test(&[0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f], DecodingError::BadMessageRootType);
+        // len < 2
+        test(&[0x91, 0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f], DecodingError::BadMessageRootArrayLength(1));
         // len > 4
-        test(&[0x95, 0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f, 0x00, 0x01, 0x02, 0x3], DecodingError::InvalidMsgPackArrayLength);
+        test(&[0x95, 0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f, 0x00, 0x01, 0x02, 0x3], DecodingError::BadMessageRootArrayLength(5));
         // got string instead of int
-        test(&[0x92, 0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f, 0x01], DecodingError::CannotConvertFromMsgPackValue);
+        test(&[0x92, 0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f, 0x01], DecodingError::BadIdFieldType);
         // message type isn't string
-        test(&[0x92, 0x01, 0x1], DecodingError::InvalidMessageTypeEncoding);
+        test(&[0x92, 0x01, 0x1], DecodingError::BadTypeFieldType);
         // unrecognized msg type
-        test(&[0x92, 0x01, 0xa6, 0x53, 0x55, 0x50, 0x4d, 0x41, 0x4e], DecodingError::UnrecognizedMessage);
-        test(&[0x93, 0x01, 0xa6, 0x53, 0x55, 0x50, 0x4d, 0x41, 0x4e, 0x01], DecodingError::UnrecognizedMessage);
-        test(&[0x93, 0x00, 0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f, 0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f], DecodingError::UnrecognizedMessage);
-        // GET_DATA msg with empty buf
-        test(&[0x93, 0x02, 0xa8, 0x47, 0x45, 0x54, 0x5f, 0x44, 0x41, 0x54, 0x41, 0xc4, 0x00], DecodingError::UnrecognizedMessage);
-        // INV message with non buf type
-        test(&[0x93, 0x02, 0xa3, 0x49, 0x4e, 0x56, 0x93, 0x01, 0x02, 0x03], DecodingError::InvalidMsgPackValueType)
+        test(&[0x92, 0x01, 0xa6, 0x53, 0x55, 0x50, 0x4d, 0x41, 0x4e], DecodingError::UnrecognizedMessageType("SUPMAN".to_string()));
+        test(&[0x93, 0x01, 0xa6, 0x53, 0x55, 0x50, 0x4d, 0x41, 0x4e, 0x01], DecodingError::UnrecognizedMessageType("SUPMAN".to_string()));
+        test(&[0x93, 0x00, 0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f, 0xa5, 0x48, 0x45, 0x4c, 0x4c, 0x4f], DecodingError::BadHelloVersionFieldType);
+        // GET_DATA msg with empty hash
+        test(&[0x93, 0x02, 0xa8, 0x47, 0x45, 0x54, 0x5f, 0x44, 0x41, 0x54, 0x41, 0xc4, 0x00], DecodingError::BadArgType("GET_DATA".to_string()));
+        // INV message with non-array type
+        test(&[0x94, 0x02, 0xa3, 0x49, 0x4e, 0x56, 0x00, 0x93, 0x01, 0x02, 0x03], DecodingError::BadArgType("INV".to_string()))
     }
 }
