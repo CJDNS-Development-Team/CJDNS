d10ecc40b17eb23ab7441fe3e90b8d073c046db6
Caleb James DeLisle <cjd@cjdns.fr>
2023-01-19 00:59:33 +0100

    Got it building again with the most recent Rust

--- cjdns-ann/src/serialized_ann.rs
+++ cjdns-ann/src/serialized_ann.rs
@@ -45,7 +45,7 @@ pub mod serialized_data {
         /// Gets signature, public signing key and signed data bytes from announcement packet and performs signature check using
         /// [crypto_sign_verify_detached](https://libsodium.gitbook.io/doc/public-key_cryptography/public-key_signatures).
         pub fn check(&self) -> Result<()> {
-            let signature = Signature::from_slice(self.get_signature_bytes()).expect("internal error: signature size != 64");
+            let signature = Signature::from_bytes(self.get_signature_bytes()).expect("internal error: signature size != 64");
             let public_sign_key = PublicKey::from_slice(self.get_pub_key_bytes()).expect("internal error: public key size != 32");
             let signed_data = self.get_signed_data();
             if verify_detached(&signature, signed_data, &public_sign_key) {
